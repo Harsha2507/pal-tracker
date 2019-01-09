21d6e27df7a75fad5b190b9834f8316035e48f93
PAL-1 <harsha_belwalkar@dell.com>
2019-01-09 09:02:29 +0530

    Committing changes

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    private String message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String mes){
+
+        this.message = mes;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+
+        return message;
+
+    }
 }
